927a1d92ca49935d276f806ed9d6970493d3da2f
Luiz Vasconcelos <luizvasconceloss@yahoo.com.br>
2018-05-22 17:11:20 -0300

    Add feature to get all keys on a path

--- lib/puppet/functions/hiera_ssm_paramstore.rb
+++ lib/puppet/functions/hiera_ssm_paramstore.rb
@@ -12,10 +12,68 @@ Puppet::Functions.create_function(:hiera_ssm_paramstore) do
   end
 
   def lookup_key(key, options, context)
-    result = paramstore_get(key, options, context)
+    ssmclient = ssm_get_connection(options)
+    key_path = context.interpolate(options['uri'] + key)
+
+    if options['get_all']
+      get_all_parameters(options, context, ssmclient)
+      if context.cache_has_key(key)
+        context.explain { "Returning value for key #{key}" }
+        return context.cached_value(key)
+      elsif context.cache_has_key(key_path)
+        context.explain { "Returning value for #{key}" }
+        return context.cached_value(key_path)
+      else
+        context.explain { "Key #{key} not found" }
+        return context.not_found
+      end
+    else
+      result = paramstore_get(key, options, context, ssmclient)
+      return result
+    end
   end
 
-  def paramstore_get(key, options, context)
+  def ssm_get_connection(options)
+    begin
+      if options['region'].nil?
+        Aws::SSM::Client.new()
+      else
+        Aws::SSM::Client.new(region: options['region'])
+      end
+    rescue Aws::SSM::Errors::ServiceError => e
+      raise Puppet::DataBinding::LookupError, "Fail to connect to aws ssm #{e.message}"
+    end
+  end
+
+  def get_all_parameters(options, context, ssmclient)
+    token = nil
+    options['recursive'] ||= false
+
+    loop do
+      begin
+        context.explain { "Getting keys on #{options['uri']} ..." }
+        data = ssmclient.get_parameters_by_path({
+          path: options['uri'],
+          recursive: options['recursive'],
+          with_decryption: true,
+          next_token: token
+        })
+        context.explain { "Adding keys on cache ..." }
+        data['parameters'].each do |k|
+          context.cache(k['name'], k['value'])
+        end
+
+        break if (data.next_token.nil?)
+        token = data.next_token
+      rescue Aws::SSM::Errors::ServiceError => e
+        raise Puppet::DataBinding::LookupError, "AWS SSM Service error #{e.message}"
+      end
+    end
+
+
+  end
+
+  def paramstore_get(key, options, context, ssmclient)
     key_path = context.interpolate(options['uri'] + key)
 
     if context.cache_has_key(key_path)
@@ -24,28 +82,18 @@ Puppet::Functions.create_function(:hiera_ssm_paramstore) do
     else
       context.explain {"Looking for #{key_path}"}
 
-      if options['region'].nil?
-        ssm = Aws::SSM::Client.new()
-      else
-        ssm = Aws::SSM::Client.new(region: options['region'])
-      end
-
       begin
-        describe = ssm.describe_parameters({filters: [{key: "Name", values: [key_path]},],})
-        if describe.parameters[0].nil?
-          return context.not_found
+        resp = ssmclient.get_parameters({
+          names: [key_path],
+          with_decryption: true
+        })
+        if !resp.parameters.empty?
+          value = resp.parameters[0].value
+          context.cache(key_path, value)
+          return value
         else
-          begin
-            resp = ssm.get_parameters({
-              names: [key_path],
-              with_decryption: true
-            })
-            value = resp.parameters[0].value
-            context.cache(key_path, value)
-            return value
-          rescue Aws::SSM::Errors::ServiceError => e
-            raise Puppet::DataBinding::LookupError, "AWS SSM Service error #{e.message}"
-          end
+          context.explain { "Key #{key_path} not found" }
+          context.not_found
         end
       rescue Aws::SSM::Errors::ServiceError => e
         raise Puppet::DataBinding::LookupError, "AWS SSM Service error #{e.message}"
